71ac62d79178f65355e178cee6e63bfc99f08abb
nnnkkk7 <kurodanaoki0711pana@gmail.com>
2021-02-02 20:02:37 +0900

    create logic

--- main.go
+++ main.go
@@ -2,17 +2,35 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	firtword := "り"
-	input()
+	firstword := "り"
+	t := input()
+	judge(firstword, t)
+
 }
 
-func input(word string) {
+func input() string {
 	stdin := bufio.NewScanner(os.Stdin)
 	stdin.Scan()
 	text := stdin.Text()
 	return text
+}
+
+func judge(word, next string) {
+	last := len(word)
+	prelast := len(word) - 3
+	laststr := word[prelast:last]
+
+	fmt.Println(laststr)
+	if strings.HasSuffix(next, laststr) {
+		fmt.Println("ok")
+	} else {
+		fmt.Println("game over")
+		os.Exit(1)
+	}
 }
