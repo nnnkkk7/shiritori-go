5e8add765bce58e1516d4d61cc8b55b3cde0dc7b
nnnkkk7 <kurodanaoki0711pana@gmail.com>
2021-02-03 07:46:21 +0900

    add comment

--- main.go
+++ main.go
@@ -8,9 +8,18 @@ import (
 )
 
 func main() {
+	fmt.Println("start!!!!!!")
 	firstword := "り"
 	t := input()
-	judge(firstword, t)
+	last := getLastStr(t)
+	nextlast := judge(firstword, last)
+	tt := input()
+	n := judge(nextlast, tt)
+	tt2 := input()
+	n2 := judge(n, tt2)
+	tt3 := input()
+	n3 := judge(n2, tt3)
+	fmt.Println(n3)
 
 }
 
@@ -21,17 +30,33 @@ func input() string {
 	return text
 }
 
-func judge(word, next string) {
-	last := len(word)
-	// 一文字３バイト
-	prelast := len(word) - 3
-	laststr := word[prelast:last]
+func judge(last, next string) string {
+	// last := len(word)
+	// // 一文字３バイト
+	// prelast := len(word) - 3
+	// // 最後の文字
+	// laststr := word[prelast:last]
 
-	fmt.Println(laststr)
-	if strings.HasSuffix(next, laststr) {
+	fmt.Println(last)
+	if strings.HasSuffix(next, last) {
 		fmt.Println("ok")
 	} else {
 		fmt.Println("game over")
 		os.Exit(1)
 	}
+	last2 := len(next)
+	// 一文字３バイト
+	prelast2 := len(next) - 3
+	// 最後の文字
+	laststrnext := next[prelast2:last2]
+	return laststrnext
+}
+
+func getLastStr(word string) string {
+	last := len(word)
+	// 一文字３バイト
+	prelast := len(word) - 3
+	// 最後の文字
+	laststr := word[prelast:last]
+	return laststr
 }
